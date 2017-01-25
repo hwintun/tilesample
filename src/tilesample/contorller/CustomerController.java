338d06555f1ca0421c76eed7cc91c91243e4126f
Hlaing Win Tun <dell@JICA_Dev_PC1>
2017-01-25 16:30:37 +0630

    Continue Update Customer module

---
+++ src/tilesample/contorller/CustomerController.java
@@ -0,0 +1,59 @@
+package tilesample.contorller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import tilesample.customer.Customer;
+import tilesample.customer.CustomerDAO;
+
+@Controller
+@RequestMapping("/c/")
+public class CustomerController {
+
+	@Autowired
+	private CustomerDAO customerDao;
+	
+	@RequestMapping(value="/c/newCustomer", method=RequestMethod.GET)
+	public ModelAndView newCustomer(ModelAndView model) {
+		Customer customer = new Customer();
+		model.addObject(customer);
+		model.setViewName("customerForm");
+		return model;
+	}
+	
+	@RequestMapping(value="/c/saveCustomer", method=RequestMethod.POST)
+	public ModelAndView saveCustomer(@ModelAttribute Customer customer) {
+		customerDao.insertCustomer(customer);
+		return new ModelAndView("redirect:/c/viewCustomer");
+	}
+	
+	@RequestMapping(value="/c/deleteCustomer", method=RequestMethod.GET)
+	public ModelAndView deleteCustomer(HttpServletRequest servlet) {
+		customerDao.deleteCustomer(Integer.parseInt(servlet.getParameter("id")));
+		return new ModelAndView("redirect:/c/viewCustomer");
+	}
+	
+	@RequestMapping(value="/c/updateCustomer", method=RequestMethod.GET)
+	public ModelAndView editCustomer(HttpServletRequest servlet) {
+		Customer customer = customerDao.selectCustomer(Integer.parseInt(servlet.getParameter("id")));
+		ModelAndView model = new ModelAndView("customerForm");
+		model.addObject("customer", customer);
+		return new ModelAndView("redirect:/c/viewCustomer");
+	}
+	
+	@RequestMapping(value="/c/listCustomer", method=RequestMethod.GET)
+	public ModelAndView listCustomer(ModelAndView model) {
+		List<Customer> listCustomer = customerDao.listCustomer();
+		model.addObject("customerList", listCustomer);
+		model.setViewName("listCustomer");
+		return model;
+	}
+}
